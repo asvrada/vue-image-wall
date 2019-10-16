b6d42e077786416e0adc5d70ab129648bc6ed328
Zijie Wu <noojeff@gmail.com>
2019-10-16 11:40:31 -0400

    Added vue tracking

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,6 @@
 <template>
     <div id="app">
         <image-wall v-bind:list-images="this.listImages"></image-wall>
-
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-114654492-4"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-114654492-4');
-</script>
-
     </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ import App from './App.vue';
 
 Vue.config.productionTip = false;
 
+import VueAnalytics from 'vue-analytics'
+Vue.use(VueAnalytics, {
+  id: 'UA-114654492-4'
+});
+
 new Vue({
   render: h => h(App),
   store
