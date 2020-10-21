c9f96f834c92d8004cb6185de2ebd3983fcf635d
Zijie Wu <noojeff@gmail.com>
2020-10-20 23:51:04 -0700

    Change main to UMD

--- src/install.js
+++ src/install.js
@@ -1,5 +1,8 @@
 import VueImageWall from './components/ImageWallWrapper';
+
 export default VueImageWall;
+
+// For browser tag
 if (typeof window !== 'undefined' && window.Vue) {
   window.Vue.component('VueImageWall', VueImageWall);
 }
